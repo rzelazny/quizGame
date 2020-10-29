c964ffa0def5baacd42338aebd1c53c3c502c7ea
Ryan Zelazny <RyanZelazny@gmail.com>
2020-10-29 16:17:01 -0400

    demo questions

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var quizSet = [
+    {
+        "question": "What is my favorite number?",
+        "answer": [
+            1,
+            2,
+            3,
+            4
+    ]
+    },
+    {
+        "question": "What is my favorite letter?",
+        "answer": [
+            "a",
+            "b",
+            "c",
+            "d"
+    ]
+    }
+]
