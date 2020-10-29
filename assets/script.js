e7e8d764b28c7543123537871361803751edeaaf
Ryan Zelazny <RyanZelazny@gmail.com>
2020-10-29 16:56:04 -0400

    setting up timer

--- assets/script.js
+++ assets/script.js
@@ -18,3 +18,30 @@ var quizSet = [
     ]
     }
 ]
+
+var timerEle = document.getElementById("timer");
+var timeLeft = 5;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+        console.log(timerEle)
+        timerEle.textContent = "Time Left: " + timeLeft;
+  
+      if(timeLeft === 0) {
+        clearInterval(timerInterval); //if not removed timer contines and goes negative
+        showLeaderboard();
+      }
+  
+    }, 1000);
+  }
+  
+  
+
+//Displays leader board when time runs out
+function showLeaderboard(){
+    timerEle.textContent = " ";
+
+    alert("Time's up!");
+}
+setTime();
