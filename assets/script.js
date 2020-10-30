288230320e4218ba6c2d271cf44a38949102aed2
Ryan Zelazny <RyanZelazny@gmail.com>
2020-10-30 15:07:56 -0400

    setting up local storage

--- assets/script.js
+++ assets/script.js
@@ -29,7 +29,7 @@ var quizSet = [
   }
 ] 
 
-//Elements that get updated
+//HTML Elements that get referenced
 var timerEle = document.getElementById("timer");
 var questionNum = document.getElementById("qNum");
 var questionEle = document.getElementById("quizQuestion")
@@ -39,9 +39,30 @@ var optionThree = document.getElementsByClassName("choice")[2];
 var optionFour = document.getElementsByClassName("choice")[3];
 var btnNextQuestion = document.getElementById("submitBtn");
 
-//Set initial time and quesition
+//Set initial time, quesition, and score
 var timeLeft = 60;
 var questionNumber = 0;
+var currentScore = 0;
+var highScores = [];
+
+//Check for saved scores
+init();
+
+function init() {
+  // Get stored scores from localStorage
+  // Parsing the JSON string to an object
+  var storedScores = JSON.parse(localStorage.getItem("scores"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedScores !== null) {
+    highScores = storedScores;
+  }
+}
+  //Store high scores for future use
+  function storeScores() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("scores", JSON.stringify(highScores));
+  }
 
 //Function starts the timer and calls showLeaderboard when time runs out
 function setTime() {
@@ -63,6 +84,8 @@ function showLeaderboard(){
     timerEle.textContent = " ";
 
     alert("Time's up!");
+
+    storeScores();
 }
 
 //Generates the next question when submitting an answer
